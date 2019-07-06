1240119b790a8c8655c5cbae032079ed97154dd1
Atomu Sato <s1252004@std1dc10.u-aizu.ac.jp>
2019-07-06 21:30:35 +0900

    Added greeting and name inquiry

--- diceGame.java
+++ diceGame.java
@@ -5,6 +5,13 @@ import java.lang.*;
 class diceGame{
 	private static final int SIDE_OF_DICE = 6;
 	public static void main(String[] args){
+		System.out.println("What is your name? ");
+		Scanner scanner = new Scanner(System.in);
+        	String name = scanner.nextLine();
+
+		System.out.println("Hello, " + name + "!");
+
+
 		System.out.println("Rolling the dice...");
 		Random rand = new Random();
 		int dice1 = rand.nextInt(SIDE_OF_DICE) + 1;
