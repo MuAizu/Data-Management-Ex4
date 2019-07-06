c5869e13a5af2e716d33d076425a58ca1a21e5a4
Atomu Sato <s1252004@std1dc10.u-aizu.ac.jp>
2019-07-06 21:57:01 +0900

    Added player count; multiple players are able to play

--- diceGame.java
+++ diceGame.java
@@ -7,26 +7,32 @@ class diceGame{
 	public static void main(String[] args){
 		Scanner scanner = new Scanner(System.in);
 
+		System.out.println("How many players?");
+		int playerNum = scanner.nextInt();
+		scanner.nextLine();
 
-		System.out.println("What is your name? ");
-        	String name = scanner.nextLine();
+		for(int i = 0; i < playerNum; i++){
+			System.out.println("What is your name? ");
+        		String name = scanner.nextLine();
 
-		System.out.println("Hello, " + name + "!");
+			System.out.println("Hello, " + name + "!");
 
 
-		System.out.println("Rolling the dice...");
-		Random rand = new Random();
-		int dice1 = rand.nextInt(SIDE_OF_DICE) + 1;
-		int dice2 = rand.nextInt(SIDE_OF_DICE) + 1;
-		int total = dice1 + dice2;
+			System.out.println("Rolling the dice...");
+			Random rand = new Random();
+			int dice1 = rand.nextInt(SIDE_OF_DICE) + 1;
+			int dice2 = rand.nextInt(SIDE_OF_DICE) + 1;
+			int total = dice1 + dice2;
 		
-		System.out.println("Die 1: " + dice1 + 
-				   "\nDie 2: " + dice2 + 
-				   "\nTotal value: " + total);
+			System.out.println("Die 1: " + dice1 + 
+					   "\nDie 2: " + dice2 + 
+					   "\nTotal value: " + total);
 
-		if(total > 7)
-			System.out.println(name + " won!");
-		else
-			System.out.println(name + " lost...");
+			if(total > 7)
+				System.out.println(name + " won!");
+			else
+				System.out.println(name + " lost...");
+			System.out.println("\n");
+		}
 	}
 }
